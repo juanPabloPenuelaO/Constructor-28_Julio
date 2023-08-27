19a1fef4123d688ce4ca0795f9012520b0c68bb3
juanPabloPenuelaO <juanppenuelaocampo@gmail.com>
2023-08-27 16:32:43 -0500

    Clase 28 de Julio

---
+++ src/Constructor/Car.java
@@ -0,0 +1,19 @@
+package Constructor;
+
+public class Car {
+    private String plate;
+    private int model;
+
+    public Car(String plate, int model) {
+        this.plate = plate;
+        this.model = model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+}
